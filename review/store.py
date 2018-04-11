0bf3701d43d1b105315d6dd8f1d3217d1de8b643
karishmasaikia <karishma.aec@gmail.com>
2018-04-11 00:39:31 -0700

    Bot

---
+++ review/store.py
@@ -0,0 +1,14 @@
+class Store:
+    #instantiate
+    #constructor function
+    def __init__(self, name):
+        #instance variable
+        self.name = name
+        
+    
+    def greet_customer(self):
+        print(f"Hello! welcome to  {self.name}")
+
+
+
+
